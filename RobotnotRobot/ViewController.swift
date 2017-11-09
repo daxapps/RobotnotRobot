aa698f7ffecbffefb125b8dd13a015e1571c6c7c
Jason Crawford <daxapps777@gmail.com>
2017-11-08 20:53:39 -0600

    Initial Commit

---
+++ RobotnotRobot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RobotnotRobot
+//
+//  Created by Jason Crawford on 11/8/17.
+//  Copyright © 2017 Dax Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
